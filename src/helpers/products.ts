826c66f31638bfc052e357368b81f9cf053871c0
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-03-20 14:22:50 -0300

    feat: add computeProductTotalPrice helper function

---
+++ src/helpers/products.ts
@@ -0,0 +1,24 @@
+import { Product } from "@prisma/client";
+
+interface ProductWithTotalPrice extends Product {
+  totalPrice: number;
+}
+
+export const computeProductTotalPrice = (product: Product): ProductWithTotalPrice => {
+  if (product.discountPercentage === 0) {
+    return {
+      ...product,
+      totalPrice: Number(product.basePrice),
+    };
+  }
+
+  const discountPrice =
+    Number(product.basePrice) * (product.discountPercentage / 100);
+
+  const totalPrice = Number(product.basePrice) - discountPrice;
+
+  return {
+    ...product,
+    totalPrice,
+  };
+};
