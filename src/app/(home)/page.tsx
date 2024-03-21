840ef14b66348109b386ef322fec36d80b09c4fa
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-03-21 14:39:05 -0300

    feat: add Footer component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,6 +4,7 @@ import { prismaClient } from "@/lib/prisma";
 import ProductList from "./components/product-list";
 import SectionTitle from "./components/section-title";
 import PromoBanner from "./components/promo-banner";
+import Footer from "@/components/ui/footer";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -31,41 +32,44 @@ export default async function Home() {
   });
 
   return (
-    <div className="flex flex-col gap-8 p-5">
-      <PromoBanner
-        src="/banner-home-01.png"
-        alt="Até 55% de desconto esse mês."
-      />
+    <>
+      <div className="flex flex-col gap-8 py-8 px-5">
+        <PromoBanner
+          src="/banner-home-01.png"
+          alt="Até 55% de desconto esse mês."
+        />
 
-      <div>
-        <Categories />
+        <div>
+          <Categories />
+        </div>
+
+        <div>
+          <SectionTitle>Ofertas</SectionTitle>
+          <ProductList products={deals} />
+        </div>
+
+        <PromoBanner
+          src="/banner-home-02.png"
+          alt="Até 55% de desconto em mouses."
+        />
+
+        <div>
+          <SectionTitle>Teclados</SectionTitle>
+          <ProductList products={keyboards} />
+        </div>
+
+        <PromoBanner
+          src="/banner-home-03.png"
+          alt="Até 20% de desconto em fones."
+        />
+
+        <div>
+          <SectionTitle>Mouses</SectionTitle>
+          <ProductList products={mouses} />
+        </div>
       </div>
-
-      <div>
-        <SectionTitle>Ofertas</SectionTitle>
-        <ProductList products={deals} />
-      </div>
-
-      <PromoBanner
-        src="/banner-home-02.png"
-        alt="Até 55% de desconto em mouses."
-      />
-
-      <div>
-        <SectionTitle>Teclados</SectionTitle>
-        <ProductList products={keyboards} />
-      </div>
-
-      <PromoBanner
-        src="/banner-home-03.png"
-        alt="Até 20% de desconto em fones."
-      />
-
-      <div>
-        <SectionTitle>Mouses</SectionTitle>
-        <ProductList products={mouses} />
-      </div>
-
-    </div>
+      
+      <Footer />
+    </>
   );
 }
---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-accent px-8 py-4 text-xs opacity-75 mt-3">
+      © 2023 Copyright <span className="font-semibold">FSW Store</span>{" "}
+    </div>
+  );
+};
+
+export default Footer;
