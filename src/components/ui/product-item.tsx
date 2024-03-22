48f251796e103229d2c8499511d66f31815fa48f
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-03-22 14:19:41 -0300

    feat: ensure user goes to product details page on product click

--- src/components/ui/product-item.tsx
+++ src/components/ui/product-item.tsx
@@ -2,53 +2,56 @@ import { ProductWithTotalPrice, priceToRealMoney } from "@/helpers/products";
 import Image from "next/image";
 import { Badge } from "./badge";
 import { ArrowDownIcon } from "lucide-react";
+import Link from "next/link";
 
 interface ProductItemProps {
   product: ProductWithTotalPrice;
 }
 const ProductItem = ({ product }: ProductItemProps) => {
   return (
-    <div className=" flex flex-col gap-4">
-      <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
-        <Image
-          src="https://i.ibb.co/2PWV5Wz/01-mx-master-3s.png"
-          width={0}
-          height={0}
-          alt={product.name}
-          sizes="100vw"
-          className="h-auto max-h-[70%] w-auto max-w-[80%]"
-          style={{ objectFit: "contain" }}
-        />
-        {product.discountPercentage > 0 && (
-          <Badge className="absolute left-2 top-2 px-2 py-[2px]">
-            <ArrowDownIcon size={14} /> {product.discountPercentage}%
-          </Badge>
-        )}
-      </div>
-
-      <div>
-        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
-          {product.name}
-        </p>
-
-        <div className="flex items-center gap-1">
-          {product.discountPercentage > 0 ? (
-            <>
-              <p className="font-semibold">
-                {priceToRealMoney(product.totalPrice)}
-              </p>
-              <p className="text-xs line-through opacity-75 overflow-hidden text-ellipsis whitespace-nowrap">
-                {priceToRealMoney(Number(product.basePrice))}
-              </p>
-            </>
-          ) : (
-            <p className="font-semibold">
-              {priceToRealMoney(Number(product.basePrice))}
-            </p>
+    <Link href={`/product/${product.slug}`}>
+      <div className=" flex flex-col gap-4">
+        <div className="relative flex h-[170px] w-full items-center justify-center rounded-lg bg-accent">
+          <Image
+            src="https://i.ibb.co/2PWV5Wz/01-mx-master-3s.png"
+            width={0}
+            height={0}
+            alt={product.name}
+            sizes="100vw"
+            className="h-auto max-h-[70%] w-auto max-w-[80%]"
+            style={{ objectFit: "contain" }}
+          />
+          {product.discountPercentage > 0 && (
+            <Badge className="absolute left-2 top-2 px-2 py-[2px]">
+              <ArrowDownIcon size={14} /> {product.discountPercentage}%
+            </Badge>
           )}
         </div>
+
+        <div>
+          <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+            {product.name}
+          </p>
+
+          <div className="flex items-center gap-1">
+            {product.discountPercentage > 0 ? (
+              <>
+                <p className="font-semibold">
+                  {priceToRealMoney(product.totalPrice)}
+                </p>
+                <p className="overflow-hidden text-ellipsis whitespace-nowrap text-xs line-through opacity-75">
+                  {priceToRealMoney(Number(product.basePrice))}
+                </p>
+              </>
+            ) : (
+              <p className="font-semibold">
+                {priceToRealMoney(Number(product.basePrice))}
+              </p>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
