679ba3232b9d5a31c20f3eef056a22813b9ba06d
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-03-25 16:30:18 -0300

    feat: ensure recommended products are shown on product details page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,8 @@
-import Image from "next/image";
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
-import ProductList from "./components/product-list";
+import ProductList from "../../components/ui/product-list";
 import SectionTitle from "./components/section-title";
 import PromoBanner from "./components/promo-banner";
-import Footer from "@/components/ui/footer";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { prismaClient } from "@/lib/prisma";
 import ProductImages from "./components/product-images";
 import ProductInfo from "./components/product-info";
 import { computeProductTotalPrice } from "@/helpers/products";
+import ProductList from "@/components/ui/product-list";
 
 interface ProductDetaisPageProps {
   params: {
@@ -16,13 +17,30 @@ const ProductDetailsPage = async ({
     where: {
       slug: slug,
     },
+    include: {
+      category: {
+        include: {
+          products: {
+            where: {
+              slug: {
+                not: slug,
+              },
+            },
+          },
+        },
+      },
+    },
   });
 
   if (!product) return null;
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 pb-14">
       <ProductImages imageUrls={product.imageUrls} name={product.name} />
       <ProductInfo product={computeProductTotalPrice(product)} />
+      <div className="px-5">
+        <p className="mb-3 font-bold uppercase text-sm">Produtos Recomendados</p>
+        <ProductList products={product.category.products} />
+      </div>
     </div>
   );
 };
