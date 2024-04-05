b700f691be540a0355f16b8d537be761eca867b2
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2024-04-05 14:10:45 -0300

    feat: ensure footer is responsive on desktop

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
     <div className="bg-accent px-8 py-4 text-xs opacity-75">
-      © 2023 Copyright <span className="font-semibold">FSW Store</span>{" "}
+      <div className="mx-auto lg:container">
+        © 2023 Copyright <span className="font-semibold">FSW Store</span>
+      </div>
     </div>
   );
 };
